03c9221a69a8435f83467c298c9ff6bfff5bdb28
efrosinina <e.efrosininaa@gmail.com>
2023-06-21 12:34:31 +0300

    gitignore

---
+++ CinemaApp/CinemaApp/Explore/Model/YouTubeSearchResponse.swift
@@ -0,0 +1,12 @@
+//
+//  YouTubeSearchResponse.swift
+//  CinemaApp
+//
+//  Created by Елизавета Ефросинина on 19/06/2023.
+//
+
+import Foundation
+
+struct YouTubeSearchResponse: Codable {
+    let items: [VideoResponseModel]
+}
